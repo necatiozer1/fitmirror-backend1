adbcf873711b1324360547bea0aa5904d6870803
necatiozer1 <necati.ozer@icloud.com>
2025-09-21 23:01:28 +0300

    Update database.py

--- database.py
+++ database.py
@@ -1,16 +1,14 @@
 from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./test.db"  # İleride PostgreSQL'e çevirebiliriz
+DATABASE_URL = "sqlite:///./fitmirror.db"  # İstersen PostgreSQL'e çevirebiliriz
 
 engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+    DATABASE_URL, connect_args={"check_same_thread": False}
 )
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
-Base = declarative_base()
-
+# Dependency
 def get_db():
     db = SessionLocal()
     try:
