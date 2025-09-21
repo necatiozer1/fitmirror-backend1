7b950f17c8dfaabdfc09b88088ce76b08d43ff3b
necatiozer1 <necati.ozer@icloud.com>
2025-09-21 22:49:04 +0300

    crud.py

--- crud.py
+++ crud.py
@@ -1,36 +1,12 @@
 from sqlalchemy.orm import Session
-from . import models
+from models import File
 
-def get_user(db: Session, uid: str):
-    return db.query(models.User).filter(models.User.uid == uid).first()
-
-def upsert_user(db: Session, uid: str, email: str | None = None):
-    user = get_user(db, uid)
-    if not user:
-        user = models.User(uid=uid, email=email)
-        db.add(user)
-    else:
-        if email:
-            user.email = email
+def create_file(db: Session, filename: str, content: bytes):
+    db_file = File(filename=filename, content=content)
+    db.add(db_file)
     db.commit()
-    db.refresh(user)
-    return user
+    db.refresh(db_file)
+    return db_file
 
-def consume_free_hq(db: Session, uid: str):
-    user = get_user(db, uid)
-    if not user:
-        return None
-    if not user.has_used_free_hq:
-        user.has_used_free_hq = True
-        db.commit()
-        db.refresh(user)
-    return user
-
-def set_subscriber(db: Session, uid: str, is_subscriber: bool):
-    user = get_user(db, uid)
-    if not user:
-        return None
-    user.is_subscriber = is_subscriber
-    db.commit()
-    db.refresh(user)
-    return user
+def get_files(db: Session):
+    return db.query(File).all()
